5e8757ad78d2f6c1ae06db4d6dde9b9eb01bba8e
John Maguire <jonnie.maguire@gmail.com>
2014-03-11 22:31:10 +0000

    Adjustment to project generation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('trainingApp')
+angular.module('trainingAppApp')
   .controller('MainCtrl', function ($scope, $http) {
     $http.get('/api/awesomeThings').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
