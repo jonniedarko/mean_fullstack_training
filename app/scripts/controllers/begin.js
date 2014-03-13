b259789e90f6b86f26448380f88718b099559fe7
John Maguire <jonnie.maguire@gmail.com>
2014-03-13 20:54:12 +0000

    finished basic Macro & Calorie Calculator

--- app/scripts/controllers/begin.js
+++ app/scripts/controllers/begin.js
@@ -5,7 +5,16 @@ var METABOLIC_CONSTANT = 5.677;
 angular.module('trainingApp')
     .controller('BeginCtrl', function($scope) {
         $scope.title = 'Calorie and Macro Calculator';
-        $scope.user = {};
+        $scope.user = {
+
+        };
+
+        $scope.getName = function(){
+            if($scope.user.name===undefined || $scope.user.name==="")
+                return "";
+
+            return $scope.user.name+"'s";
+        }
         $scope.activityLevels = [{
                                     title: "Little or No Exercise"
                                     ,description: "You are Lazy and avoid exercise"
@@ -24,45 +33,75 @@ angular.module('trainingApp')
                                 ,{
                                     title: "Heavey Exercise"
                                     ,description: "You Exercise 6-7 times a week"
-                                    ,value: 1.2
+                                    ,value: 1.725
                                 }
                                 ,{
                                     title: "Professional Athlete"
-                                    ,description: "You Exercise approx <b>2 times a Day<b/>"
-                                    ,value: 1.2
+                                    ,description: "You Exercise approx 2 times a Day"
+                                    ,value: 1.9
                                 }
                                 ];
 
         $scope.goals = {
                         current: {
                             title:"Mainteance"
-                            ,calories: 100
-                            ,protein: 101
-                            ,fats: 102
-                            ,carbs:103
+                            ,calories: 0
+                            ,protein: 0
+                            ,fats: 0
+                            ,carbs:0
                         }
                         ,loss: {
                             title:"Fat Loss"
-                            ,calories: 200
-                            ,protein: 201
-                            ,fats: 202
-                            ,carbs:203
+                            ,calories: 0
+                            ,protein: 0
+                            ,fats: 0
+                            ,carbs:0
                         }
                         ,gain:{
                             title:"Muscle Gain"
-                            ,calories: 300
-                            ,protein: 301
-                            ,fats: 302
-                            ,carbs:303
+                            ,calories: 0
+                            ,protein: 0
+                            ,fats: 0
+                            ,carbs:0
                         }
                     }
-                    $scope.setGoals=function(){
+                    console.log($scope.goals);
+                        $scope.updateNutrition=function(){
+                            if(($scope.user.sex === "male" || $scope.user.sex === "female") && $scope.user.weight>0 && $scope.user.height>0 && $scope.user.age>0 && $scope.user.activityLevel>0){
+                            $scope.goals.current.calories = parseFloat($scope.user.bmr * $scope.user.activityLevel)|| 0;
+                            $scope.goals.loss.calories = $scope.goals.current.calories * 0.85 || 0;
+                            $scope.goals.gain.calories = $scope.goals.current.calories * 1.15 || 0;
+
+                            $scope.goals.current.protein = $scope.user.weight*2.204 || 0;
+                            $scope.goals.loss.protein = $scope.user.weight*2.204 || 0;
+                            $scope.goals.gain.protein = $scope.user.weight*2.204*1.5 || 0;
+
+                            $scope.goals.current.carbs = ($scope.goals.current.calories*0.3)/4 || 0; 
+                            $scope.goals.loss.carbs = ($scope.goals.loss.calories*0.15)/4 || 0;
+                            $scope.goals.gain.carbs = ($scope.goals.gain.calories*0.3)/4 || 0;
+
+                            //=(calories-(protein*4)-(carbs*4))/9
+                            $scope.goals.current.fats = ($scope.goals.current.calories -($scope.goals.current.protein*4)-($scope.goals.current.carbs*4))/9 || 0;
+                            $scope.goals.loss.fats = ($scope.goals.loss.calories -($scope.goals.loss.protein*4)-($scope.goals.loss.carbs*4))/9 || 0;
+                            $scope.goals.gain.fats = ($scope.goals.gain.calories -($scope.goals.gain.protein*4)-($scope.goals.gain.carbs*4))/9 || 0;
+                        }
 
                     };
         $scope.getMetaRate = function(){
-            var  meta = 88.362 + (13.397 * parseFloat(  $scope.user.weight) )
+            //=88.362+(13.397*weight)+(5.799*height)-(5.677*age)
+          var meta; 
+          if($scope.user.sex === "male"){
+                    meta = (88.362 + (13.397 * parseFloat(  $scope.user.weight) )
                             +(5.799 * parseFloat(  $scope.user.height) )
-                            -(5.677 * parseFloat(  $scope.user.age) ) || null;
+                            -(5.677 * parseFloat(  $scope.user.age) ) || 0 ); //3071.487 
+                    }else if($scope.user.sex === "female"){
+                        meta = (447.593 + (9.247 * parseFloat(  $scope.user.weight) )
+                            +(3.098 * parseFloat(  $scope.user.height) )
+                            -(4.33 * parseFloat(  $scope.user.age) ) || 0 );
+                    }
+                    else meta = null;
+                        //=447.593+(9.247*C4)+(3.098*C5)-(4.33*C6)
+         $scope.user.bmr= meta;
 
             return (!isNaN(meta) && (meta!=null)) ? parseFloat(meta).toFixed(2) : "Please Fill Out the above Form";
         };
